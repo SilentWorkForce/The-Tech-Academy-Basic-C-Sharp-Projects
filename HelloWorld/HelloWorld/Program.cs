a8013d22def57cb32d4b353f71a60c4214eece29
Christopher <christopherstam@yahoo.com>
2019-04-13 11:06:14 -0700

    Added Exercise

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine ("Welcome to Acme Accounting Systems" +
+            Environment.NewLine +
+            "Remember, we’re “accounting” on you!");
+        Console.ReadLine();
+        }
+    }
+
