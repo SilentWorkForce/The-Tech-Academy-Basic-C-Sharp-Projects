ffcb5e07f43a1ca4153d532ff68d9f02ba04c048
Christopher <christopherstam@yahoo.com>
2019-04-14 10:19:24 -0700

    added new project

---
+++ C#/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The Tech Academy" +
+                Environment.NewLine +
+                "Student Daily Report");
+            Console.WriteLine("What course are you on?");
+            Console.ReadLine();
+            Console.WriteLine("What page number?");
+            Console.ReadLine();
+            Console.WriteLine("Do you need help with anything?" +
+                Environment.NewLine +
+                "Please answer 'true' or 'false'.");
+            Console.ReadLine();
+            Console.WriteLine("Were there any positive experinces you'd like to share?");
+            Console.ReadLine();
+            Console.WriteLine("Is there any other feedback you’d like to provide? ");
+            Console.ReadLine();
+            Console.WriteLine("How many hours did you study today?");
+            Console.ReadLine();
+            Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            Console.ReadLine();
+                
+        }
+     
+    }
+}
