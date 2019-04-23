66c91224aa49ed24a2e521d55226bf628e87197f
SilentWorkForce <christopherstam@yahoo.com>
2019-04-23 08:24:18 -0700

    added Class,Objects,Methods

---
+++ classMethod/classMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user what number they want to do the math operations on.
+            Console.WriteLine("What number would you like to perfrom calculations on?");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            //Display the returned integer to the screen.
+            Console.WriteLine(MyMethod.addTwo(input));
+            Console.WriteLine(MyMethod.subTwo(input));
+            Console.WriteLine(MyMethod.divTwo(input));
+
+            Console.Read();
+
+
+        }
+    }
+}
