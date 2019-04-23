babd15cffe97ec1b7aa14ef012fe1ce8d7fd9621
SilentWorkForce <christopherstam@yahoo.com>
2019-04-23 10:01:54 -0700

    Added exercise 115

---
+++ Lab115/Lab115/MyClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//create a method that takes two integers as parameters.Make one of them optional.
+//    Have the method do a math operation and return a integer result.
+
+
+namespace Lab115
+{
+    class MyClass
+    {
+        public MyClass()
+        {
+
+        }
+
+        public int myMethod(int x, int y = 4)
+        {
+            return x + y;
+        }
+
+
+
+    }
+}
---
+++ Lab115/Lab115/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            //Ask the user to input two numbers, one at a time.
+            //Let them know they need not enter anything for the second number.
+
+            Console.WriteLine("input at least one whole number to be used, The second input used may be blank.");
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+
+            //Call the method in MyClass, passing in the one or two numbers entered.
+
+            int outcome;
+            if (String.IsNullOrWhiteSpace(second))
+                outcome = myClass.myMethod(Convert.ToInt32(first));
+            else
+                outcome = myClass.myMethod(Convert.ToInt32(first), Convert.ToInt32(second));
+
+            Console.WriteLine("the value returned from the method W/ optional parameter: " + outcome);
+            Console.Read();
+
+
+
+
+        }
+    }
+}
