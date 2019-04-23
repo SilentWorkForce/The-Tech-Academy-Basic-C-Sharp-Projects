1ac1b21415d7006e15e123633b3f08e3feefead1
SilentWorkForce <christopherstam@yahoo.com>
2019-04-23 09:19:46 -0700

    Added exercise 113

---
+++ Lab113/Lab113/MyClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//create a void method that takes two integers as parameters.
+//Have the method do a math operation on the first integer and display the second integer to the screen.
+
+namespace Lab113
+{
+    class MyClass
+    {
+        public MyClass()
+        {
+
+        }
+
+        public void myMethod(int x, int y)
+        {
+            x = x + 2;
+            Console.WriteLine(y);
+
+        }
+    }
+}
---
+++ Lab113/Lab113/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyClass myClass = new MyClass();
+
+            //Call the method in the class, passing in two numbers.
+            myClass.myMethod(1, 2);
+
+            //Call the method in the class, specifying the parameters by name.
+            myClass.myMethod(y: 1, x: 30);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
