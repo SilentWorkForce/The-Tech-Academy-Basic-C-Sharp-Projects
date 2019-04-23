c02367a69e7192fa5398da7b18339d9e4e227841
SilentWorkForce <christopherstam@yahoo.com>
2019-04-23 09:35:47 -0700

    Added exercise 114

---
+++ Lab114/Lab114/MyClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab114
+{
+    class MyClass
+    {
+
+        public MyClass()
+        {
+
+        }
+
+        //do a math operation to it and then return the answer as an integer
+
+        public int myMethod(int num)
+        {
+            return num * 2;
+        }
+
+        //do a different math operation to it and then return the answer as an integer
+
+        public int myMethod(decimal d)
+        {
+            return Convert.ToInt32(d % 5);
+        }
+
+        //call the third method, passing in a string that equates to an integer
+
+        public int myMethod(string s)
+        {
+            int final = Convert.ToInt32(s) + 1000;
+            return final;
+        }
+    }
+}
---
+++ Lab114/Lab114/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            MyClass myClass = new MyClass();
+
+            //call the first method, passing in an integer, such as 12.Then display the result to the screen.
+
+            Console.WriteLine(myClass.myMethod(12));
+            
+            //call the second method, passing in a decimal.Display the result to the screen.
+
+           Console.WriteLine(myClass.myMethod(1.5m));
+          
+            //call the third method, passing in a string that equates to an integer
+
+            Console.WriteLine(myClass.myMethod("24"));
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
