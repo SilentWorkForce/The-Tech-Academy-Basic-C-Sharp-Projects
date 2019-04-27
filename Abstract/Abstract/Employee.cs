7442c872d749c14fdbc9bc6f10b0276509474b1c
SilentWorkForce <christopherstam@yahoo.com>
2019-04-27 09:14:28 -0700

    Added lab 124

---
+++ Abstract/Abstract/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Employee : Person
+    {
+
+        int Id = 0;
+
+
+
+        public Employee(string FirstName, string LastName) : base(FirstName, LastName)
+        {
+
+        }
+
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + "[" + base.FirstName + " " + base.LastName + "]");
+        }
+
+       
+
+
+    }
+}
