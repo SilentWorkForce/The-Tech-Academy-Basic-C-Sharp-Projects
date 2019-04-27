8d1802adf8880b2d9a83a8ac037834f75e9cdce2
SilentWorkForce <christopherstam@yahoo.com>
2019-04-27 07:49:30 -0700

    Added Lab120

---
+++ Inheritance/Inheritance/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Employee : Person
+    {
+        int Id = 0;
+
+        //Calls the base constructor (Person)
+
+        public Employee(string FirstName, string LastName) : base(FirstName, LastName)
+        {
+            //Blank
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Person
+    {
+        //Create a class called Person and give it two properties, each of data type string. One called FirstName, the other LastName.
+
+        string FirstName;
+        string LastName;
+
+        public Person(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+
+        }
+
+        //Give this class a void method called SayName() that takes no parameters and simply writes the person's full name to the console in the format of: "Name: [full name]".
+
+        public void SayName()
+        {
+
+            Console.WriteLine("Name: " + "[" + FirstName + " " + LastName + "]");
+
+
+        }
+
+   
+
+
+
+    }
+
+}
