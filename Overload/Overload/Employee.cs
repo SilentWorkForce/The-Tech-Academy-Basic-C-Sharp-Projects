3b357a7e26f57d6693b2a9d309c1c2a330ad21c0
SilentWorkForce <christopherstam@yahoo.com>
2019-04-27 10:58:05 -0700

    Added Lab 129

---
+++ Overload/Overload/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload
+{
+    class Employee : Person
+    {
+
+        public int Id = 0;
+
+        //Calls the base constructor (Person)
+        public Employee(string FirstName, string LastName) : base(FirstName, LastName)
+        {
+
+        }
+
+        public static bool operator ==(Employee a, Employee b)
+        {
+            return a.Id == b.Id;
+        }
+
+        public static bool operator !=(Employee a, Employee b)
+        {
+            return a.Id != b.Id;
+        }
+
+        public void Quit()
+        {
+            Console.WriteLine("Press Enter to Exit... BYE!");
+            Console.Read();
+            Environment.Exit(0);
+        }
+
+        //Override base method 
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + "[" + base.FirstName + " " + base.LastName + "]");
+        }
+
+    }
+}
---
+++ Overload/Overload/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee("Same","Person");
+            Employee emp2 = new Employee("Same","Person");
+            Employee emp3 = new Employee("Different","Person");
+            emp3.Id = 1;
+
+            Console.WriteLine("Comparing first two employees: ");
+            Console.WriteLine("Result: " + Convert.ToString(emp1 == emp2));
+            Console.WriteLine("Comparing second and third employees: ");
+            Console.WriteLine("Result: " + Convert.ToString(emp3 == emp2));
+            Console.ReadLine();
+
+        }
+    }
+}
