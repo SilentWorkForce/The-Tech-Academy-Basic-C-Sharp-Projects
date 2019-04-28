e73f01edc82657e682c46dcb1207b752d832a46d
SilentWorkForce <christopherstam@yahoo.com>
2019-04-28 07:44:34 -0700

    Added Lab 131

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Employee<G> : Person
+    {
+        public int Id = 0;
+        public List<G> Things = new List<G>();
+
+        public Employee()
+        {
+
+        }
+        public void Quit()
+        {
+            Console.WriteLine("Press enter to exit...");
+            Console.Read();
+            Environment.Exit(0);
+
+        }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + "[" + base.FirstName + " " + base.LastName + "]");
+        }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> empString = new Employee<string>();
+            empString.Things.Add("first");
+            empString.Things.Add("second");
+            empString.Things.Add("third");
+            empString.Things.Add("fourth");
+            empString.Things.Add("fifth");
+            Employee<int> intString = new Employee<int>();
+            intString.Things.Add(1);
+            intString.Things.Add(2);
+            intString.Things.Add(3);
+            intString.Things.Add(4);
+            intString.Things.Add(5);
+
+            Console.WriteLine("Employee.Thing of type String: ");
+            empString.Things.ForEach (x => { Console.WriteLine(x); });
+            Console.WriteLine("Employee.Thing of type int: ");
+            intString.Things.ForEach(x => { Console.WriteLine(x); });
+
+            Console.Read();
+
+        }
+    }
+}
