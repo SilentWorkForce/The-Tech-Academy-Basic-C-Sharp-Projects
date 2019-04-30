915e30e42937cd38ab8a8293623f633efae51d34
Christopher <47716553+SilentWorkForce@users.noreply.github.com>
2019-04-30 07:03:28 -0700

    Updated Program

--- infiniteArray/infiniteArray/Program.cs
+++ infiniteArray/infiniteArray/Program.cs
@@ -107,8 +107,7 @@ namespace infiniteArray
              * foreach loop that evaluates each item in the list, and displays a message showing the string
              * and whether or not it has already appeared in the list.*/
             match = false;
-            List<string> repeats = new List<string>();
-            stringList = new List<string>();
+            List<string> stringList = new List<string>();
             stringList.Add("same string");
             stringList.Add("same string");
             stringList.Add("same string");
@@ -117,15 +116,20 @@ namespace infiniteArray
             stringList.Add("different string");
             stringList.Add("12-0391-2093");
 
-            Console.WriteLine("String|Repeat?");
-            foreach (string str in stringList)
+            bool match = false;
+            List<string> repeats = new List<string>();
+           
+            foreach (string item in stringList)
             {
-                foreach (string inner in repeats)
-                    if (inner.Equals(str))
-                        match = true;
-                Console.WriteLine(str + " | " + match);
-                repeats.Add(str);
-                match = false;
+                if (!repeats.Contains(item))
+                {
+                    repeats.Add(item);
+                    Console.WriteLine("This <<" + item + ">> appears for the first time.");
+                }
+                else
+                {
+                    Console.WriteLine("This <<" + item + ">> has ALREADY APPEARED in the list.");
+                }
             }
 
             Console.Read();
