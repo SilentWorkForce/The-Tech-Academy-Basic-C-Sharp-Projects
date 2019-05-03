dbb26b7f58af7493c3d572805b7fe102c47aa348
SilentWorkForce <christopherstam@yahoo.com>
2019-05-03 07:41:22 -0700

    Added Lab 154

---
+++ FileIO/FileIO/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ask user for number
+
+            Console.WriteLine("Enter a number: ");
+            string input = Console.ReadLine();
+
+            //Log the text
+
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Sample.txt"))
+            {
+                file.Write(input);
+            }
+
+            //print the text back
+
+            using (StreamReader file = new StreamReader("Sample.txt"))
+            {
+                Console.WriteLine("File contents: ");
+                Console.WriteLine(file.ReadToEnd());
+            }
+
+            Console.Read();
+
+        }
+    }
+}
