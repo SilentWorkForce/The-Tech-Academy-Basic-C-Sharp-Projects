b9d066f794f86e237cbc18dd0a05c5a134e22730
SilentWorkForce <christopherstam@yahoo.com>
2019-05-03 09:31:28 -0700

    Added Lab 162

---
+++ ConstructorChaining/ConstructorChaining/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChaining
+{
+    class Person
+    {
+
+        string FirstName;
+        string LastName;
+
+        public Person() : this("Christopher", "Stam") { }
+
+        public Person(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: " + "[" + FirstName + " " + LastName + "]");
+
+        }
+
+
+
+    }
+}
---
+++ ConstructorChaining/ConstructorChaining/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChaining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            const string constString = "Constant.";
+            Console.WriteLine(constString);
+            var num = 5m;
+            Console.WriteLine("var num is of type: " + num.GetType());
+            var str = "String.";
+            Console.WriteLine("var str is of type: " + str.GetType());
+
+            Person person = new Person();
+            person.SayName();
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
