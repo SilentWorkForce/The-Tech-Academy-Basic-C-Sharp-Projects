8c4e82a9e6ca073a9ebc01f42d71ab8e02ce9310
SilentWorkForce <christopherstam@yahoo.com>
2019-05-03 10:11:37 -0700

    Added Lab 165

---
+++ TryCatch/TryCatch/Program.cs
@@ -0,0 +1,45 @@
+﻿
+/*Try Catch step 165 for The Tech Academy*/
+using System;
+
+
+namespace TryCatchStep165
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+
+
+            //Ask the user for his age.
+            //Display the year user born.
+    Start:
+        Console.WriteLine("Please enter your age:");
+        int input = 0;
+        try
+        {
+           //Display appropriate error messages if user enters zero or negative numbers.
+
+           input = Convert.ToInt32(Console.ReadLine());
+            if (input < 1)
+                throw new InvalidOperationException();
+        }
+        catch (InvalidOperationException)
+        {
+            Console.WriteLine("Please do not enter a value lower than 1");
+            goto Start;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Please only enter digits.");
+            goto Start;
+        }
+
+        Console.WriteLine("User was born in: " + DateTime.Now.AddYears(-input).Year);
+        Console.ReadLine();
+
+
+
+        }
+    }
+}
